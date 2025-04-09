453c8e4d62131d8cfa65e90e4620db0059b2452d
simonbjerkas <simon.bjerkas10@gmail.com>
2025-04-09 11:39:40 +0200

    removed seeding and added alias path

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,9 @@
-import { convexQuery } from "@convex-dev/react-query";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
-import { api } from "../../convex/_generated/api";
+import { convexQuery } from '@convex-dev/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute } from '@tanstack/react-router';
+import { api } from '@/convex/_generated/api';
 
-export const Route = createFileRoute("/")({
+export const Route = createFileRoute('/')({
   component: Home,
 });
 
